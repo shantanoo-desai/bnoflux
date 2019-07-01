8b4545cd9a9f6a1bc3e6b44d6f877481cb273088
Shan Desai <shantanoo.desai@gmail.com>
2019-07-01 18:25:02 +0200

    Bump v1.1 -> v1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 
 setup(name='bnoflux',
-    version=1.1,
+    version=1.2,
     description='Extract IMU Values from BNO055 and store them in InfluxDB via UDP',
     long_description=readme(),
     url='https://github.com/shantanoo-desai/bnoflux',
